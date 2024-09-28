d50938c2198b33aa1dfd8d568dc2df1f712531e8
Jón Bjarni Bjarnason <jon@centroid.is>
2024-09-28 07:50:25 +0000

    popup works

--- lib/widgets/nav_dropdown.dart
+++ lib/widgets/nav_dropdown.dart
@@ -4,7 +4,7 @@ import 'package:beamer/beamer.dart';
 import '../models/menu_item.dart';
 import '../app_colors.dart';
 
-class NavDropdown extends StatefulWidget {
+class NavDropdown extends StatelessWidget {
   final MenuItem menuItem;
   final bool isSelected;
   final VoidCallback onMenuItemSelected;
@@ -17,104 +17,68 @@ class NavDropdown extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<NavDropdown> createState() => _NavDropdownState();
-}
-
-class _NavDropdownState extends State<NavDropdown> {
-  bool _isOpen = false;
-
-  void _toggleDropdown() {
-    setState(() {
-      _isOpen = !_isOpen;
-    });
-  }
-
-  void _closeDropdown() {
-    setState(() {
-      _isOpen = false;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     print('NavDropdown build');
-    print('isSelected: ${widget.isSelected}');
-    print('menuItem: ${widget.menuItem}');
-    print('isOpen: $_isOpen');
-    print('children: ${widget.menuItem.children}');
+    print('isSelected: $isSelected');
+    print('menuItem: ${menuItem.label}');
+    print('children: ${menuItem.children?.map((e) => e.label).toList()}');
 
-    return Stack(
-      children: [
-        // Dropdown Button
-        IconButton(
-          icon: Icon(
-            widget.menuItem.icon,
-            color: widget.isSelected
-                ? AppColors.selectedItemColor
-                : AppColors.unselectedItemColor,
-          ),
-          tooltip: widget.menuItem.hoverText,
-          onPressed: _toggleDropdown,
-        ),
-        // Dropdown Menu
-        if (_isOpen)
-          Positioned(
-            top: 48, // Adjust based on icon size
-            child: GestureDetector(
-              onTap: _closeDropdown,
-              behavior: HitTestBehavior.translucent,
-              child: Material(
-                elevation: 4,
-                color: AppColors.backgroundColor,
-                child: Container(
-                  width: 200,
-                  decoration: BoxDecoration(
-                    color: AppColors.backgroundColor,
-                    border: Border.all(color: AppColors.borderColor),
+    return PopupMenuButton<MenuItem>(
+      icon: Icon(
+        menuItem.icon,
+        color: isSelected
+            ? AppColors.selectedItemColor
+            : AppColors.unselectedItemColor,
+      ),
+      tooltip: menuItem.hoverText,
+      onSelected: (MenuItem selectedItem) {
+        Beamer.of(context).beamToNamed(selectedItem.path);
+        onMenuItemSelected();
+      },
+      itemBuilder: (BuildContext context) {
+        return menuItem.children?.map((MenuItem child) {
+              if (child.children != null && child.children!.isNotEmpty) {
+                return PopupMenuItem<MenuItem>(
+                  child: ExpansionTile(
+                    leading:
+                        Icon(child.icon, color: AppColors.primaryIconColor),
+                    title: Text(
+                      child.label,
+                      style: TextStyle(color: AppColors.primaryTextColor),
+                    ),
+                    children: child.children!.map((MenuItem grandChild) {
+                      return ListTile(
+                        leading: Icon(grandChild.icon,
+                            color: AppColors.primaryIconColor),
+                        title: Text(
+                          grandChild.label,
+                          style: TextStyle(color: AppColors.primaryTextColor),
+                        ),
+                        onTap: () {
+                          Beamer.of(context).beamToNamed(grandChild.path);
+                          Navigator.pop(context); // Close the popup menu
+                          onMenuItemSelected();
+                        },
+                      );
+                    }).toList(),
                   ),
-                  child: _buildMenu(widget.menuItem.children ?? []),
-                ),
-              ),
-            ),
-          ),
-      ],
+                );
+              } else {
+                return PopupMenuItem<MenuItem>(
+                  value: child,
+                  child: ListTile(
+                    leading:
+                        Icon(child.icon, color: AppColors.primaryIconColor),
+                    title: Text(
+                      child.label,
+                      style: TextStyle(color: AppColors.primaryTextColor),
+                    ),
+                  ),
+                );
+              }
+            }).toList() ??
+            [];
+      },
     );
-  }
-
-  Widget _buildMenu(List<MenuItem> items) {
-    return ListView(
-      shrinkWrap: true,
-      padding: EdgeInsets.zero,
-      children: items.map(_buildMenuItem).toList(),
-    );
-  }
-
-  Widget _buildMenuItem(MenuItem item) {
-    if (item.children != null && item.children!.isNotEmpty) {
-      return ExpansionTile(
-        leading: Icon(item.icon, color: AppColors.primaryIconColor),
-        title: Text(
-          item.label,
-          style: TextStyle(color: AppColors.primaryTextColor),
-        ),
-        tilePadding: EdgeInsets.symmetric(horizontal: 16.0),
-        childrenPadding: EdgeInsets.only(left: 32.0),
-        children: item.children!.map(_buildMenuItem).toList(),
-      );
-    } else {
-      return ListTile(
-        leading: Icon(item.icon, color: AppColors.primaryIconColor),
-        title: Text(
-          item.label,
-          style: TextStyle(color: AppColors.primaryTextColor),
-        ),
-        hoverColor: AppColors.hoverColor,
-        onTap: () {
-          Beamer.of(context).beamToNamed(item.path);
-          _closeDropdown();
-          widget.onMenuItemSelected();
-        },
-      );
-    }
   }
 }
