527a7919d6bc1f30c9fe89e3547e59e7a6b81274
Jón Bjarni <jon@centroid.is>
2025-01-07 14:31:59 +0000

    Add developing flake file and instructions on how to use in vscode (#5)

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+description = "Bleeding edge Flutter version supporting native assets";
+inputs = {
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  flake-utils.url = "github:numtide/flake-utils";
+};
+outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in
+    {
+      devShell =
+        with pkgs; mkShell rec {
+          buildInputs = [
+            flutter
+          ];
+        };
+    });
+}
