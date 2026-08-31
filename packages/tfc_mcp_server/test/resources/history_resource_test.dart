dc7a7ca6ebe8ab10a00ebe9cb8d70b23d76379fd
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2026-08-31 13:17:15 +0000

    test(07-09): drop the identity argument from three resource suites
    - config_snapshot, drawings_index and history resource tests
    - seven inline EnvOperatorIdentity constructions removed, nothing substituted
    - the env_operator_identity import dropped from each
    - 13/13 pass, test count unchanged

--- packages/tfc_mcp_server/test/resources/history_resource_test.dart
+++ packages/tfc_mcp_server/test/resources/history_resource_test.dart
@@ -4,7 +4,6 @@ import 'package:drift/drift.dart' hide isNotNull, isNull;
 import 'package:test/test.dart';
 
 import 'package:tfc_mcp_server/src/database/server_database.dart';
-import 'package:tfc_mcp_server/src/identity/env_operator_identity.dart';
 import 'package:tfc_mcp_server/src/server.dart';
 import '../helpers/mock_alarm_reader.dart';
 import '../helpers/mock_mcp_client.dart';
@@ -85,12 +84,7 @@ void main() {
             ),
           );
 
-      final identity = EnvOperatorIdentity(
-        environmentProvider: () => {'TFC_USER': 'op1'},
-      );
-
       server = TfcMcpServer(
-        identity: identity,
         database: db,
         stateReader: stateReader,
         alarmReader: alarmReader,
@@ -167,11 +161,7 @@ void main() {
       final emptyDb = ServerDatabase.inMemory();
       await emptyDb.customStatement('SELECT 1');
 
-      final emptyIdentity = EnvOperatorIdentity(
-        environmentProvider: () => {'TFC_USER': 'op1'},
-      );
       final emptyServer = TfcMcpServer(
-        identity: emptyIdentity,
         database: emptyDb,
         stateReader: MockStateReader(),
         alarmReader: MockAlarmReader(),
